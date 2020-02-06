9eef238a6b5c81c0b0d9a482bf4f937830ec3367
Mohamed Wagdy <mohamed.wagdy@virgingates.com>
2020-02-06 12:58:18 +0200

    connecting mongodb

--- src/main/kotlin/components/user/UserRoutes.kt
+++ src/main/kotlin/components/user/UserRoutes.kt
@@ -2,10 +2,21 @@ package components.user
 
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respondText
+import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.get
 import io.ktor.routing.route
+import io.ktor.routing.get
+import io.ktor.routing.post
+
+import org.litote.kmongo.reactivestreams.*  //NEEDED! import KMongo reactivestreams extensions
+import org.litote.kmongo.coroutine.* //NEEDED! import KMongo coroutine extensions
+
+
+val client = KMongo.createClient().coroutine //use coroutine extension
+//val database = client.getDatabase("test") //normal java driver usage
+
 
 fun Route.userRoutes() {
 
@@ -13,6 +24,25 @@ fun Route.userRoutes() {
 
         get("") {
             call.respondText("all users", ContentType.Text.Html)
+        }
+
+        get("/list") {
+            val users = client.getDatabase("sample_app")
+                    .getCollection<User>("users2")
+                    .find()
+                    .toList()
+            call.respond(HttpStatusCode.OK, users)
+        }
+
+        post<RegisterRequest>("/register") { request ->
+            val user = User(
+                    userName = request.userName,
+                    password = request.password
+            )
+            client.getDatabase("sample_app")
+                    .getCollection<User>("users2")
+                    .insertOne(user)
+            call.respond(HttpStatusCode.OK)
         }
 
     }
--- src/main/kotlin/ktor/MainApp.kt
+++ src/main/kotlin/ktor/MainApp.kt
@@ -1,13 +1,20 @@
 package ktor
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import components.user.userRoutes
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.jackson.jackson
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
     install(DefaultHeaders)
     install(CallLogging)
     routing {
